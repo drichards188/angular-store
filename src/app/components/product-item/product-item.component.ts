eb1d9b6b32f350f97fd06ea29c7f0a29478a8f73
drichards188 <github@cxoprotect.com>
2022-12-07 16:44:31 -0700

    added product details foundations

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -7,6 +7,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 
 export class ProductItemComponent implements OnInit {
+  @Input() id: number = 0;
   @Input() name: string = "Name";
   @Input() price: number = 0;
   @Input() imageUrl: string = '';
@@ -15,9 +16,6 @@ export class ProductItemComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // this.name = "Hotel Room";
-    // this.price = 89;
-    // this.url = 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'
 
   }
 }
